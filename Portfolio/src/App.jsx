277f4aaa446730610b57718f158d0d0721d42f1c
VIKAS S S <vikas.ss2023ece@sece.ac.in>
2026-01-13 16:19:09 +0530

    initial

---
+++ Portfolio/src/App.jsx
@@ -0,0 +1,14 @@
+import { useState } from 'react'
+import reactLogo from './assets/react.svg'
+import viteLogo from '/vite.svg'
+import './App.css'
+
+function App() {
+  return (
+    <h1 className="text-4xl font-bold text-blue-600">
+      Tailwind is working 🚀
+    </h1>
+  );
+}
+
+export default App;
